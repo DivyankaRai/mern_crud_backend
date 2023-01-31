388998db7c1c4a24ab5ce7baef24386d33c5078f
Divyanka Rai <raidivyanka40@gmail.com>
2023-01-31 12:52:41 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: '.env'})
 require("./db/connection")
 const cors = require("cors")
 const router = require("./routes/router")
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
